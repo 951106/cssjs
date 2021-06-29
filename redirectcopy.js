e3c6ae693d202f891065f24926850086b2d1afd8
Duong Nguyen <86592505+951106@users.noreply.github.com>
2021-06-29 15:10:44 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://grandcenterquynhon-hungthinh.blogspot.com/'>https://grandcenterquynhon-hungthinh.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://haigiangmerryland-propertyx.blogspot.com/'>https://haigiangmerryland-propertyx.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
